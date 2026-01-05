f92e99e41abbb144efe00dcda7368f14cfe5ac84
Cristina Viejó <krissvinti@gmail.com>
2026-01-05 18:58:23 +0100

    basictest1resolved

--- src/basic.js
+++ src/basic.js
@@ -1,13 +1,16 @@
 function welcome(){
     //Escribe tu código aquí
+    return "welcome"
 };
 
 function evenOrOdd(n){
     //Escribe tu código aquí
+    return (n % 2 ===0)? "even":"odd"
 };
 
 function greeting(name) {
     //Escribe tu código aquí
+    return "Hello"+ name;
 };
 
 export { evenOrOdd, greeting, welcome }
--- tests/basic.test.js
+++ tests/basic.test.js
@@ -8,12 +8,13 @@ describe('Las variables en Javascript', () => {
     });
     it('pueden contener números', () => {
         // Añade la línea que falta
+        const b = 14.64;
 
         expect(b).toBe(14.64);
     });
     it('pueden contener booleanos', () => {
         // Añade la línea que falta
-
+        const c = false;
         expect(c).toBeFalsy();
     });
 });
@@ -23,13 +24,13 @@ describe('Comparaciones en Javascript', () => {
     it("usando ===", () => {
         const number = 20;
         //completa la asignación usando number y el comparador ===
-        const expresion = '???';
+        const expresion = (number === 10);
         expect(expresion).toBeFalsy();
     });
     it("usando !==", () => {
         const language = "Javascript";
         //completa la asignación usando language y el comparador !==
-        const expresion = '???';
+        const expresion = (language !== "Javascript");
         expect(expresion).toBeFalsy();
     });
 });
@@ -38,20 +39,20 @@ describe('Comparaciones en Javascript', () => {
 describe("Operaciones aritméticas", () => {
     it("Usando suma", () => {
         //completa la asignación de a y b
-        const a = '???';
-        const b = '???';
+        const a = '50';
+        const b = '50';
         expect(a + b).toBe(100);
     });
     it("Usando multiplicación", () => {
         //completa la asignación de a y b
-        const a = '???';
-        const b = '???';
+        const a = '1';
+        const b = '13';
         expect(a * b).toBe(13);
     });
     it("Usando división", () => {
         //completa la asignación de a y b
-        const a = '???';
-        const b = '???';
+        const a = '5';
+        const b = '2';
         expect(a / b).toBe(2.5);
     });
 });
@@ -61,14 +62,14 @@ describe('Algebra booleana', () => {
     it("Usando el operador AND (&&)", () => {
         const a = true;
         //completa la asignación de b
-        const b = '???';
+        const b = 'true';
         const expression = a && b;
         expect(expression).toBeTruthy();
     });
     it("Usando el operador OR (||)", () => {
         const a = false;
         //completa la asignación de b
-        const b = '???';
+        const b = 'false';
         const expression = a || b;
         expect(expression).toBeFalsy();
     });
